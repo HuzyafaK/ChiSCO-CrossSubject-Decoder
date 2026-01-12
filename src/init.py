7933d165160db784f1dee0b7b43f25e7e57db9ab
Muhammad Huzyafa Khokhar <mhk7960@nyu.edu>
2026-01-12 18:15:17 +0500

    Create init.py

---
+++ src/init.py
@@ -0,0 +1,11 @@
+"""
+ChiSCO Cross-Subject Decoder
+
+First cross-subject contrastive learning system for sentence-level imagined speech.
+
+Dataset: Zhang et al. (2024) - ChiSCO, Scientific Data
+"""
+
+__version__ = "0.1.0"
+__author__ = "Muhammad Huzyafa Khokhar"
+__email__ = "mhk7960@nyu.edu"
